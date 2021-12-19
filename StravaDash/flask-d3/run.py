540bb62c2b4dce8ac6e6bebb21a68163cbc8b886
ty-martz <jacksontmartz@gmail.com>
2021-12-19 18:04:37 -0500

    add pie chart data

--- StravaDash/flask-d3/run.py
+++ StravaDash/flask-d3/run.py
@@ -2,33 +2,19 @@ from flask import Flask, jsonify, render_template
 import csv
 import pandas as pd
 import numpy as np
+import scripts.clean_activities as pp
 
 app = Flask(__name__)
 
 @app.route('/')
-def preprocess_data(df):
-    df = df[['Activity ID', 'Activity Date', 'Activity Name', 'Activity Type', 'Elapsed Time',
-           'Elapsed Time.1', 'Distance', 'Distance.1', 'Max Heart Rate', 'Relative Effort',
-           'Moving Time', 'Max Speed', 'Average Speed', 'Elevation Gain',
-           'Elevation Loss', 'Elevation Low', 'Elevation High', 'Max Grade', 'Average Grade',
-           'Average Heart Rate', 'Calories']]
-    df.columns = [i.lower().replace(' ', '_') for i in df.columns]
-    df['time_of_day'] = [i.split(',')[2].strip() for i in df['activity_date']]
-    df['distance_m'] = df['distance.1'] / 1.609
-    df['time_minutes'] = df['elapsed_time'] / 60
-    df['max_speed_mph'] = df['max_speed'] * 2.237
-    df['average_speed_mph'] = df['average_speed'] * 2.237
-    df = df.rename(columns={'distance.1':'distance_km', 'elapsed_time.1':'time_sec'})
-
-    return df
-
-@app.route('/get_piechart_data')
-def get_piechart_data():
+def index():
     data = pd.read_csv('StravaDash/data/activities.csv')
-    df = preprocess_data(data)
-    active_counts_table = df['activity_type'].value_counts().reset_index()
-    active_counts_table.columns = ['Activity', 'Count']
+    df = pp.preprocess_data(data)
+    activity_cnt = pp.col_val_counts(df, 'activity_type')
+    json_data = pp.jsonify_counts(activity_cnt)
+    return render_template('templates/index.html', data=json_data)
 
-    return jsonify(Activities=active_counts_table['Activity'], Count=active_counts_table['Count'])
+if __name__ == '__main__':
+    app.run()
 
 
