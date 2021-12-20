d11aeab5b4c9cd50e0cf18bb8bb7066e6dff344f
ty-martz <jacksontmartz@gmail.com>
2021-12-19 20:49:10 -0500

    general updates

--- StravaDash/flask-d3/run.py
+++ StravaDash/flask-d3/run.py
@@ -1,24 +1,20 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template
 import pandas as pd
-import numpy as np
 import clean_activities as pp
-import json
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/')
 def index():
     data = pd.read_csv('../data/activities.csv')
     df = pp.preprocess_data(data)
     activity_cnt = pp.col_val_counts(df, 'activity_type')
-    json_data = pp.jsonify_counts(activity_cnt)
     dd = {}
     for i in range(len(activity_cnt)):
         dd[activity_cnt.index.tolist()[i]] = activity_cnt.iloc[i]
-    #dd = json.dumps(json_data)
-    #dd = jsonify(activity_cnt)
     return render_template('index.html', data=dd)
-    #return json_data
+
 
 if __name__ == '__main__':
     app.run()
